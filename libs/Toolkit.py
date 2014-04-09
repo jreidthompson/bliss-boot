d6f2790c79e5f1debf330dc9d77027fe265ca9c3
Jonathan Vasquez <jvasquez1011@gmail.com>
2014-04-08 23:53:14 -0400

    relicensed to the more permissive Apache License 2.0, and other minor stuff

--- libs/Toolkit.py
+++ libs/Toolkit.py
@@ -1,8 +1,18 @@
-# Copyright (C) 2014 Jonathan Vasquez <jvasquez1011@gmail.com>
-#
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+"""
+Copyright 2014 Jonathan Vasquez <jvasquez1011@gmail.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at:
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 
 import os
 import sys
@@ -76,7 +86,7 @@ class Toolkit(object):
 		self.ewarn("---------------------------")
 		self.ewarn("| " + other.pname + " - v" + other.pversion)
 		self.ewarn("| Author: " + other.pcontact)
-		self.ewarn("| Distributed under the " + other.plicense)
+		self.ewarn("| Licensed under the " + other.plicense)
 		self.ewarn("---------------------------")
 
 	def print_usage(self):
