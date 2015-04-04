f381494269d798a8a48c6d1cc0245f36f14471bc
Jonathan Vasquez <jvasquez1011@gmail.com>
2015-04-04 17:47:09 -0400

    relicensed back to the mpl 2.0

--- libs/Variables.py
+++ libs/Variables.py
@@ -1,5 +1,8 @@
 # Copyright 2014-2015 Jonathan Vasquez <jvasquez1011@gmail.com>
-# Licensed under the Simplified BSD License which can be found in the LICENSE file.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import libs.ConfigLoader as ConfigLoader
 
@@ -10,5 +13,5 @@ name = "Bliss Boot"
 author = "Jonathan Vasquez"
 email = "jvasquez1011@gmail.com"
 contact = author + " <" + email + ">"
-version = "2.1.1"
-license = "Simplified BSD License"
+version = "2.1.2"
+license = "MPL 2.0"
